7179ecf771925afc18534a80d9f2d17335d4b1db
FrilundAleksei <frilundag@gmail.com>
2022-08-15 20:39:02 +0300

    решены 3 задачи

---
+++ task025/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+
+using static System.Console;
+Write("Ввведите число А: ");
+int A=int.Parse(ReadLine());
+int sum=GetSum(A);
+WriteLine($"Сумма от 1 до {A} = {sum}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int GetSum(int number)
+{
+int result=0;
+for (int i=1;i<=number; i++)
+{
+result+=i;
+}
+return result;
+
+}
---
+++ task026/Program.cs
@@ -0,0 +1,25 @@
+﻿// **Задача 26:**
+
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+WriteLine(FunctionCount (Convert.ToInt32(ReadLine())));
+
+int FunctionCount (int n)
+{
+    int result = 0;
+
+while (n > 0)
+    {
+        n /= 10;
+        result ++;
+    }
+
+    return result;
+}
